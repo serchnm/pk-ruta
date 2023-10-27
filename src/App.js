30dfee6d15e3fa6edcce4955e7a94445a208531b
Sergio N <snavarro@arkusnexus.com>
2023-10-27 16:21:15 -0700

    fixing route

--- src/App.js
+++ src/App.js
@@ -17,8 +17,8 @@ function App() {
   return (
     <div className="App App-background">
       <PokemonHeader key="headerComponent" />
-      <Route path="/pokedex"><PokedexView/></Route>
-      <Route path="/"><div className='columns'>
+      <Route path="/pk-ruta/pokedex"><PokedexView/></Route>
+      <Route path="/pk-ruta"><div className='columns'>
         <div className='column is-one-fifth'><Menu key="menuComponent" /></div>
         <div className='column is-three-fifths'>{pokemonObject}</div>
       </div></Route>
